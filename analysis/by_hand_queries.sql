2e9ecd675fddc9042b01291e7d3a3cd57346f50b
Rahul Dhodapkar <rahuldhodapkar@Rahuls-MacBook-Pro.local>
2019-06-24 08:53:52 -0400

    add first-pass expression analysis

--- analysis/by_hand_queries.sql
+++ analysis/by_hand_queries.sql
@@ -70,3 +70,70 @@ INNER JOIN
   donor
 ON
   k."DonorNum" = donor."Sample";
+
+
+
+/* Get number of cells with ERCC transcripts by donor and tissue type */
+select SUBSTRING("Gene", 1, 4) as "Code", "DonorNum", "TissueType", "Group",
+        count(*),
+         repeat('■', (  100.0
+                      * count(*)
+                      / sum(count(*)) over()
+                     )::integer
+               ) as pct
+    from expression_donor
+group by SUBSTRING("Gene", 1, 4), "DonorNum", "TissueType", "Group"
+HAVING
+  SUBSTRING("Gene", 1, 4) = 'ERCC';
+
+/* list all materialized views */
+select schemaname as schema_name,
+       matviewname as view_name
+from pg_matviews
+order by schema_name,
+         view_name;
+
+/* Get number of cells from blood for MS and HC, excluding 9 + 10 */
+SELECT 
+    "Group",
+    SUM("n") as NumCells
+FROM 
+    (SELECT 
+        "DonorNum", COUNT(DISTINCT "CellName") as n 
+    FROM 
+        (SELECT "DonorNum", "CellName", "TissueType"
+          FROM variant WHERE "DonorNum" NOT IN (9,10)) as clean_vars
+    GROUP BY 
+        "DonorNum", "TissueType" 
+    HAVING 
+        "TissueType"='B') as a
+INNER JOIN 
+    donor ON a."DonorNum"=donor."Sample"
+GROUP BY
+    "Group";
+
+
+
+/* Get number of cells with FOXP3 transcripts by donor and tissue type */
+select "Gene", "DonorNum", "TissueType", "Group",
+        count(*),
+         repeat('■', (  100.0
+                      * count(*)
+                      / sum(count(*)) over()
+                     )::integer
+               ) as pct
+    from expression_donor_unclean
+group by "Gene", "DonorNum", "TissueType", "Group"
+HAVING
+  "Gene" = 'FOXP3';
+
+/* list all materialized views */
+select schemaname as schema_name,
+       matviewname as view_name
+from pg_matviews
+order by schema_name,
+         view_name;
+
+
+
+
